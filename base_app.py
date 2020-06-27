f3b90bcf7cc32b791aab143f8df1f6700f1e37d6
Menzi Mchunu <menzi639@gmail.com>
2020-06-27 11:32:27 +0200

    image not shown

--- base_app.py
+++ base_app.py
@@ -615,7 +615,7 @@ def main():
 
 	if selection == "Instruction of use":
 		st.title("Instructions")
-		st.markdown('<p>When the application opens the first page you will see is the prediction page. <img src="images/prediction_p.png" width="200" height="80">\
+		st.markdown('<p>When the application opens the first page you will see is the prediction page. <img src="images/prediction_p.png" >\
 		 Here as a user you will put your tweet on the text box and then choose a machine learning algorithm to classify the tweet and then when it has been selected then you can press the classify button .\
 		You can find more about machine learning and the algorithm used in this application on the "About Machine Learning App" page.\
 		On the top right you will see an arrow and when you click it you will see a sidebar .\
